2151060860bdc3085f51866e550d2981498c0399
komangsuryasedana <komangsuryasedana@mail.com>
2024-01-19 12:34:00 +0800

    edit line 4 & 2

--- controller/auth.js
+++ controller/auth.js
@@ -1,7 +1,7 @@
 const { Users, User_Details } = require("../models");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const JWT_SECRET = process.env.JWT_SECRET
+const JWT_SECRET = process.env.JWT_SECRET || "INI RAHASIA"
 
 
 async function register(req, res, next) {
--- middleware/verifyAccess.js
+++ middleware/verifyAccess.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET || "INI RAHASIA";
 
 async function verifyAccesToken(req, res, next) {
   try {
