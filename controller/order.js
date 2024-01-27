4e18be1d8d4ad2dea94dc1e3e2844b980d9941fc
komangsuryasedana <komangsuryasedana@mail.com>
2024-01-27 14:10:19 +0800

    menambahkan transaction di createOrder

--- controller/order.js
+++ controller/order.js
@@ -1,42 +1,50 @@
+const { sequelize } = require("sequelize");
 const { ErrorResponse, SuccessResponse } = require("../middleware/handlerMiddleware");
 const { Users,Order, Items } = require("../models");
 const { getFormattedDate } = require("../utils/date");
-const dateFormat = getFormattedDate()
+const dateNow = getFormattedDate()
+
 
 async function createOrder(req, res, next) {
+  const transaction = await sequelize.transaction();
+  
   try {
-    const user_id = +req.params.user_id 
+    const user_id = +req.params.user_id;
     const { item_id, quantity } = req.body;
 
-    const findUser = await Users.findByPk(user_id)
+    const findUser = await Users.findByPk(user_id, { transaction });
     if (!findUser) {
       const response = new ErrorResponse("User Not Found!", 404);
       res.status(404).json(response);
+      await transaction.rollback();
+      return;
     }
-    
-    const item = await Items.findOne({ where: { id: item_id } });
+
+    const item = await Items.findOne({ where: { id: item_id }, transaction });
     if (!item) {
-      const response = new ErrorResponse("Id Items Not Found", 404);
+      const response = new ErrorResponse("Item Not Found", 404);
       res.status(404).json(response);
+      await transaction.rollback();
+      return;
     }
-    const total_amount = item.item_price * quantity
 
-    const total_order_price = total_amount * 1.1
+    const total_amount = item.item_price * quantity;
+    const total_order_price = total_amount * 1.1;
 
-    const order = await Order.create({
-      user_id,
-      quantity,
-      status_order: "Pending",
-      date_order_placed: dateFormat,
-      total_order_price 
-    });
-
-    await order.addItems(item, {
-      through: {
-        quantity: quantity,
-        total_amount: total_amount,
+    const order = await Order.create(
+      {
+        user_id,
+        quantity,
+        status_order: "Pending",
+        date_order_placed: dateNow,
+        total_order_price 
       },
-    });
+      { transaction }
+    );
+
+    await order.addItems(item, { through: { quantity, total_amount }, transaction });
+
+    await transaction.commit();
 
     const response = new SuccessResponse(
       "Order created successfully",
@@ -45,9 +53,11 @@ async function createOrder(req, res, next) {
     );
     res.status(201).json(response);
   } catch (error) {
+    await transaction.rollback();
     next(error);
   }
 }
+
 
 async function orderSuccess(req, res, next) {
   try {
@@ -57,7 +67,7 @@ async function orderSuccess(req, res, next) {
       const response = new ErrorResponse("Order Not Found!", 404)
       return res.status(404).json(response);
     } else {
-      updateOrder.date_order_paid = dateFormat
+      updateOrder.date_order_paid = dateNow
       updateOrder.status_order = "Success"
 
       await updateOrder.save();
